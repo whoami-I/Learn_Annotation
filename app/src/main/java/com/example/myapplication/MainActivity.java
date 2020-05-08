1aa3ba142ee4d25724f61b8839c11dad807162dc
haigen.zhu <haigen.zhu@tcl.com>
2020-05-08 21:17:58 +0800

    add view binding and click binding

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,16 +16,33 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.tv)
     public TextView textView;
 
+    @BindView(R.id.tv1)
+    public TextView textView1;
+
+    @BindView(R.id.tv2)
+    public TextView textView2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //HelloWorld.main();
         AutoBind.getInstance().inject(this);
         textView.setText("hahaha");
+        textView1.setText("hahaha111");
+        textView2.setText("hahaha222");
     }
     @OnClick(R.id.tv)
     public void click(View v){
         Log.d("TAG","hello");
+    }
+
+    @OnClick(R.id.tv1)
+    public void click1(View v){
+        Log.d("TAG","hello11");
+    }
+
+    @OnClick(R.id.tv2)
+    public void click2(View v){
+        Log.d("TAG","hello22");
     }
 }
--- ioc-compiler/src/main/java/com/example/ioc_compiler/BindAnnotation.java
+++ ioc-compiler/src/main/java/com/example/ioc_compiler/BindAnnotation.java
@@ -5,8 +5,10 @@ import com.example.ioc_annotation.Hello;
 import com.example.ioc_annotation.IBindHelper;
 import com.example.ioc_annotation.OnClick;
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 
 import java.io.IOException;
@@ -30,6 +32,9 @@ import javax.tools.Diagnostic;
 
 @AutoService(Processor.class)
 public class BindAnnotation extends AbstractProcessor {
+    private static final ClassName VIEW = ClassName.get("android.view", "View");
+
+
     Map<TypeElement, Set<ViewInfo>> bindViewMap = new HashMap<>();
     private Elements mElementsUtils;  // Element处理工具类
     private Filer filer;
@@ -61,49 +66,88 @@ public class BindAnnotation extends AbstractProcessor {
             Set<? extends Element> onClickElements = roundEnvironment.getElementsAnnotatedWith(OnClick.class);
             categoryBindView(bindViewElements);
             categoryOnClick(onClickElements);
-            Set<TypeElement> typeElements = bindViewMap.keySet();
-            for (TypeElement typeElement : typeElements) {
-                String className = typeElement.getSimpleName().toString();
-                String packageName = mElementsUtils.getPackageOf(typeElement).getQualifiedName().toString();
-                Set<ViewInfo> viewInfos = bindViewMap.get(typeElement);
-                MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder("inject").addAnnotation(Override.class)
-                        .addModifiers(Modifier.PUBLIC)
-                        .returns(void.class)
-                        .addParameter(Object.class, "target")
-                        //.addCode(typeElement.getQualifiedName()+" a = ("+typeElement.getQualifiedName()+")target;\n");
-                        .addStatement("$L a = ($L)target", typeElement.getQualifiedName(), typeElement.getQualifiedName());
-
-                for (ViewInfo viewInfo : viewInfos) {
-                    StringBuilder sb = new StringBuilder();
-                    //使用findviewbyid来找到相应的view
-                    methodBuilder.addStatement("a.$L = a.findViewById($L)",viewInfo.name,viewInfo.id);
-                    ;
-                }
-                MethodSpec build = methodBuilder.build();
-                // 创建类
-                TypeSpec classZ = TypeSpec.classBuilder(className + "$$Bind")
-                        .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-                        .addSuperinterface(IBindHelper.class)
-                        .addMethod(build)
-                        .build();
-
-                try {
-                    // 生成 com.wzgiceman.viewinjector.HelloWorld.java
-                    JavaFile javaFile = JavaFile.builder(packageName, classZ)
-                            .addFileComment(" This codes are generated automatically. Do not modify!")
-                            .build();
-                    //　生成文件
-                    javaFile.writeTo(filer);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
+            generateCode();
         }
         return true;
     }
 
+    private void generateCode() {
+        Set<TypeElement> typeElements = bindViewMap.keySet();
+        for (TypeElement typeElement : typeElements) {
+            String className = typeElement.getSimpleName().toString();
+            String packageName = mElementsUtils.getPackageOf(typeElement).getQualifiedName().toString();
+            Set<ViewInfo> viewInfos = bindViewMap.get(typeElement);
+            MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder("inject").addAnnotation(Override.class)
+                    .addModifiers(Modifier.PUBLIC)
+                    .returns(void.class)
+                    .addParameter(Object.class, "target")
+                    //.addCode(typeElement.getQualifiedName()+" a = ("+typeElement.getQualifiedName()+")target;\n");
+                    .addStatement("$L a = ($L)target", typeElement.getQualifiedName(), typeElement.getQualifiedName());
+            //使用findviewbyid来找到相应的view
+            methodBuilder.addStatement("$T view = null", VIEW);
+            for (ViewInfo viewInfo : viewInfos) {
+                methodBuilder.addStatement("view = a.findViewById($L)", viewInfo.id);
+                if (viewInfo.name != null) {
+                    //绑定view
+                    methodBuilder.addStatement("a.$L = ($L)view", viewInfo.name,(ClassName)(viewInfo.typeName));
+                }
+                if (viewInfo.methodName != null) {
+                    //绑定click
+                    methodBuilder.addStatement("view.setOnClickListener(new View.OnClickListener() {\n" +
+                            "            @Override\n" +
+                            "            public void onClick(View v) {\n" +
+                            "               a.$L(v);\n" +
+                            "                \n" +
+                            "            }\n" +
+                            "        });", viewInfo.methodName);
+                }
+            }
+            MethodSpec build = methodBuilder.build();
+            // 创建类
+            TypeSpec classZ = TypeSpec.classBuilder(className + "$$Bind")
+                    .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                    .addSuperinterface(IBindHelper.class)
+                    .addMethod(build)
+                    .build();
+
+            try {
+                // 生成 com.wzgiceman.viewinjector.HelloWorld.java
+                JavaFile javaFile = JavaFile.builder(packageName, classZ)
+                        .addFileComment(" This codes are generated automatically. Do not modify!")
+                        .build();
+                //　生成文件
+                javaFile.writeTo(filer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     private void categoryOnClick(Set<? extends Element> onClickElements) {
+        for (Element onClickElement : onClickElements) {
+            TypeElement enclosingElement = (TypeElement) onClickElement.getEnclosingElement();
+            Set<ViewInfo> viewInfos = bindViewMap.get(enclosingElement);
+            if (viewInfos == null) {
+                viewInfos = new HashSet<>();
+                bindViewMap.put(enclosingElement, viewInfos);
+            }
+            OnClick annotation = onClickElement.getAnnotation(OnClick.class);
+            int value = annotation.value();
+            String name = onClickElement.getSimpleName().toString();
+            ViewInfo info = null;
+            for (ViewInfo viewInfo : viewInfos) {
+                if (viewInfo.id == value) {
+                    info = viewInfo;
+                    break;
+                }
+            }
+            if (info == null) {
+                info = new ViewInfo();
+                info.id = value;
+                viewInfos.add(info);
+            }
+            info.methodName = name;
+        }
     }
 
     private void categoryBindView(Set<? extends Element> bindViewElements) {
@@ -117,17 +161,33 @@ public class BindAnnotation extends AbstractProcessor {
             BindView annotation = bindViewElement.getAnnotation(BindView.class);
             int value = annotation.value();
             String name = bindViewElement.getSimpleName().toString();
-            viewInfos.add(new ViewInfo(value, name));
+            ViewInfo info = null;
+            for (ViewInfo viewInfo : viewInfos) {
+                if (viewInfo.id == value) {
+                    info = viewInfo;
+                    break;
+                }
+            }
+            if (info == null) {
+                info = new ViewInfo();
+                info.id = value;
+                viewInfos.add(info);
+            }
+            info.name = name;
+            info.typeName=TypeName.get(bindViewElement.asType());
         }
     }
 
     private static class ViewInfo {
         private int id;
+        //变量的类型
+        private TypeName typeName;
+        //变量的名称
         private String name;
+        private String methodName;
 
-        public ViewInfo(int id, String name) {
-            this.id = id;
-            this.name = name;
+
+        public ViewInfo() {
         }
     }
 }
